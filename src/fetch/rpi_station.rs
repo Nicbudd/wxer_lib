50b4dba23e05aa44d68b8ce68a3c77ae4f12cdd9
nicbudd <nicbudd@nicbudd.com>
2024-11-03 00:00:12 -0400

    0.9.3 fixed some other issues

--- src/fetch/rpi_station.rs
+++ src/fetch/rpi_station.rs
@@ -29,68 +29,79 @@ pub async fn import(station_url: &str, date: DateTime<Utc>) -> Result<StationDat
         .text()
         .await?;
 
+    // dbg!(&resp);
+
     let csv_string = String::from("time,indoor_temp,outdoor_temp,rh,dewpoint,raw_pres,mslp\n") + resp.as_str();
 
     let mut reader = csv::Reader::from_reader(csv_string.as_bytes());
 
     let mut local_db = BTreeMap::new();
 
-    for record in reader.deserialize() {
-        let record: RawStationEntry = record?;
-
-        let time_string = String::from(record.time) + "Z";
-        let mut dt = time_string.parse::<DateTime<Utc>>()?;
-        dt = dt - Duration::seconds(dt.second() as i64 + 60); // to account for when the data collection ends
-
-        let indoor = WxEntryLayer {
-            layer: Layer::Indoor,
-            height_agl: Some(2.0),
-            height_msl: Some(altitude),
-            temperature: record.indoor_temp,
-            dewpoint: None,
-            pressure: record.raw_pres,
-            wind_direction: None,
-            wind_speed: None,
-            visibility: None,
-        };
-
-        let near_surface = WxEntryLayer {
-            layer: Layer::NearSurface,
-            height_agl: Some(2.0),
-            height_msl: Some(altitude),
-            temperature: record.outdoor_temp,
-            dewpoint: record.dewpoint,
-            pressure: record.raw_pres,
-            wind_direction: None,
-            wind_speed: None,
-            visibility: None,
-        };
-
-        let mut layers = HashMap::new();
-
-        layers.insert(Layer::Indoor, indoor);
-        layers.insert(Layer::NearSurface, near_surface);
-
-        let entry: WxEntry = WxEntry {
-            date_time: dt,
-            station: station.clone(),
-
-            layers,
-            
-            cape: None,
-            skycover: None,
-            raw_metar: None,
-            precip_today: None,
-            precip: None,
-            precip_probability: None,
-            present_wx: None,
-            altimeter: None,
-        };
-
-        local_db.insert(dt, entry);
+    for (i, record) in reader.deserialize().enumerate() {
+        match try_parse_entry(record, altitude, station.clone()) {
+            Ok((dt, entry)) => {local_db.insert(dt, entry);},
+            Err(e) => {eprintln!("Error parsing entry {i}: {e}");}
+        }
     } 
 
+    // dbg!();
+
     Ok(local_db)
+}
+
+fn try_parse_entry(record: Result<RawStationEntry, csv::Error>, altitude: f32, station: Station) -> Result<(DateTime<Utc>, WxEntry)> {
+    let record: RawStationEntry = record?;
+
+    let time_string = String::from(record.time) + "Z";
+    let mut dt = time_string.trim().chars().filter(|x| x != &'\0').collect::<String>().parse::<DateTime<Utc>>()?;
+    dt = dt - Duration::seconds(dt.second() as i64 + 60); // to account for when the data collection ends
+
+    let indoor = WxEntryLayer {
+        layer: Layer::Indoor,
+        height_agl: Some(2.0),
+        height_msl: Some(altitude),
+        temperature: record.indoor_temp,
+        dewpoint: None,
+        pressure: record.raw_pres,
+        wind_direction: None,
+        wind_speed: None,
+        visibility: None,
+    };
+
+    let near_surface = WxEntryLayer {
+        layer: Layer::NearSurface,
+        height_agl: Some(2.0),
+        height_msl: Some(altitude),
+        temperature: record.outdoor_temp,
+        dewpoint: record.dewpoint,
+        pressure: record.raw_pres,
+        wind_direction: None,
+        wind_speed: None,
+        visibility: None,
+    };
+
+    let mut layers = HashMap::new();
+
+    layers.insert(Layer::Indoor, indoor);
+    layers.insert(Layer::NearSurface, near_surface);
+
+    let entry: WxEntry = WxEntry {
+        date_time: dt,
+        station: station,
+
+        layers,
+        
+        cape: None,
+        skycover: None,
+        raw_metar: None,
+        precip_today: None,
+        precip: None,
+        precip_probability: None,
+        present_wx: None,
+        altimeter: None,
+    };
+
+    Ok((dt, entry))
 }
 
 #[derive(Debug, Deserialize)]
